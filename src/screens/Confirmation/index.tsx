82bd1578f0e27d407fa718bc7fe7d0ff3ed70fc1
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-17 20:28:04 -0300

    refactor: extract component for all pages

---
+++ src/screens/Confirmation/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StatusBar, useWindowDimensions } from 'react-native';
+import { ConfirmButton } from '../../components/ConfirmButton';
+
+import { Container, Content, Title, Message, Footer } from './styles';
+
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+import {
+  NavigationProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+
+interface Params {
+  title: string;
+  message: string;
+  nextScreen: string;
+}
+
+export function Confirmation() {
+  const { width } = useWindowDimensions();
+  const navigation: NavigationProp<any> = useNavigation();
+  const route = useRoute();
+  const { title, message, nextScreen } = route.params as Params;
+  function handleConfirm() {
+    navigation.navigate(nextScreen);
+  }
+  return (
+    <Container>
+      <StatusBar
+        barStyle={'light-content'}
+        translucent
+        backgroundColor={'transparent'}
+      />
+      <LogoSvg width={width} />
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>{title}</Title>
+        <Message>{message}</Message>
+      </Content>
+      <Footer>
+        <ConfirmButton title={'OK'} onPress={handleConfirm} />
+      </Footer>
+    </Container>
+  );
+}

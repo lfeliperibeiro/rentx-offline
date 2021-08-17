87a4a04516b9c2b7e163fa11e587d9adb0d4d81e
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-17 20:42:42 -0300

    feat: create second step signup

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import {
   Container,
   Header,
@@ -7,30 +7,62 @@ import {
   SubTitle,
   Form,
   FormTitle,
-} from "./styles";
-import { BackButton } from "../../../components/BackButton";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { Bullet } from "../../../components/Bullet";
-
-import { Button } from "../../../components/Button";
+} from './styles';
+import { BackButton } from '../../../components/BackButton';
 import {
+  NavigationProp,
+  useNavigation,
+  // useRoute,
+} from '@react-navigation/native';
+import { Bullet } from '../../../components/Bullet';
+
+import { Button } from '../../../components/Button';
+import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-} from "react-native";
-import { InputPassword } from "../../../components/InputPassword";
-import { useTheme } from "styled-components";
+} from 'react-native';
+import { InputPassword } from '../../../components/InputPassword';
+import { useTheme } from 'styled-components';
+
+// interface Params {
+//   user: {
+//     name: string;
+//     email: string;
+//     driveLicense: string;
+//   };
+// }
 
 export function SignUpSecondStep() {
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
   const theme = useTheme();
   const navigation: NavigationProp<any> = useNavigation();
+  // const route = useRoute();
+  // const { user } = route.params as Params;
 
   function handleBack() {
     navigation.goBack();
   }
 
+  function handleRegister() {
+    if (!password || !passwordConfirm) {
+      return Alert.alert('Informe a senha e a confimação');
+    }
+    if (password !== passwordConfirm) {
+      return Alert.alert('As senhas não são iguais');
+    }
+    navigation.navigate('Confirmation', {
+      nextScreen: 'SignIn',
+      title: 'Conta Criada!',
+      message: `Agora é só fazer login\ne aproveitar`,
+    });
+  }
+
   return (
-    <KeyboardAvoidingView behavior={"position"} enabled>
+    <KeyboardAvoidingView behavior={'position'} enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <Container>
           <Header>
@@ -40,14 +72,28 @@ export function SignUpSecondStep() {
               <Bullet />
             </Steps>
           </Header>
-          <Title>Crie sua {"\n"}conta</Title>
-          <SubTitle>Faça seu cadastro de {"\n"}forma rápida e fácil</SubTitle>
+          <Title>Crie sua {'\n'}conta</Title>
+          <SubTitle>Faça seu cadastro de {'\n'}forma rápida e fácil</SubTitle>
           <Form>
             <FormTitle>2. Senha</FormTitle>
-            <InputPassword iconName={"lock"} placeholder={"Senha"} />
-            <InputPassword iconName={"lock"} placeholder={"Repetir senha"} />
+            <InputPassword
+              iconName={'lock'}
+              placeholder={'Senha'}
+              onChangeText={setPassword}
+              value={password}
+            />
+            <InputPassword
+              iconName={'lock'}
+              placeholder={'Repetir senha'}
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
+            />
           </Form>
-          <Button title={"Cadastrar"} color={theme.colors.success} />
+          <Button
+            title={'Cadastrar'}
+            color={theme.colors.success}
+            onPress={handleRegister}
+          />
         </Container>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>

a8e041ea5f0654568ddc6fc200aa3f59110779ab
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-17 20:41:45 -0300

    feat: create first step signup

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import {
   Container,
   Header,
@@ -7,32 +7,90 @@ import {
   SubTitle,
   Form,
   FormTitle,
-} from "./styles";
-import { BackButton } from "../../../components/BackButton";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { Bullet } from "../../../components/Bullet";
+} from './styles';
+import { BackButton } from '../../../components/BackButton';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
+import * as Yup from 'yup';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 export function SignUpFirstStep() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [driveLicense, setDriveLicense] = useState('');
   const navigation: NavigationProp<any> = useNavigation();
 
   function handleBack() {
     navigation.goBack();
   }
 
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object().shape({
+        driveLicense: Yup.string().required('CNH obrigatória'),
+        email: Yup.string()
+          .email('E-mail inválido')
+          .required('Email obrigatório'),
+        name: Yup.string().required('Nome obrigatório'),
+      });
+      const data = { name, email, driveLicense };
+      await schema.validate(data);
+      navigation.navigate('SignUpSecondStep', { user: data });
+    } catch (e) {
+      if (e instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', e.message);
+      }
+    }
+    navigation.navigate('SignUpSecondStep');
+  }
+
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
-      <Title>Crie sua {"\n"}conta</Title>
-      <SubTitle>Faça seu cadastro de {"\n"}forma rápida e fácil</SubTitle>
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+    <KeyboardAvoidingView behavior={'position'} enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+          <Title>Crie sua {'\n'}conta</Title>
+          <SubTitle>Faça seu cadastro de {'\n'}forma rápida e fácil</SubTitle>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input
+              iconName={'user'}
+              placeholder={'Nome'}
+              onChangeText={setName}
+              value={name}
+            />
+            <Input
+              iconName={'mail'}
+              placeholder={'E-mail'}
+              keyboardType={'email-address'}
+              autoCapitalize={'none'}
+              onChangeText={setEmail}
+              value={email}
+            />
+            <Input
+              iconName={'credit-card'}
+              placeholder={'CNH'}
+              keyboardType={'numeric'}
+              onChangeText={setDriveLicense}
+              value={driveLicense}
+            />
+          </Form>
+          <Button title={'Próximo'} onPress={handleNextStep} />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
